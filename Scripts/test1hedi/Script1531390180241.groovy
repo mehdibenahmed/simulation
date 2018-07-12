8bf42b2b6bfc14ffaee966ba222c43e7b5169327
Mehdi-BENAHMED <Mehdi-BENAHMED@PC38.decade.fr>
2018-07-12 12:27:18 +0200

    test1hedi modif

--- Scripts/test1hedi/Script1531390180241.groovy
+++ Scripts/test1hedi/Script1531390180241.groovy
@@ -19,6 +19,6 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKe
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 
-'testhedi\r\n'
+'testhedi1254\r\n'
 WebUI.delay(0)
 
