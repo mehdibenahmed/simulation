adab046a24ab02de0f4fdeba5f2d817d9fb16199
Mehdi-BENAHMED <Mehdi-BENAHMED@PC38.decade.fr>
2018-07-12 12:46:48 +0200

    tt

--- Scripts/test1hedi/Script1531390180241.groovy
+++ Scripts/test1hedi/Script1531390180241.groovy
@@ -19,7 +19,7 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKe
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 
-'kkktesthediiiiiiiiiiiiiii\r\n\r\n'
+'testhediiggg\r\n\r\n'
 WebUI.delay(0)
 
 WebUI.navigateToUrl('')
@@ -28,7 +28,7 @@ WebUI.maximizeWindow()
 
 WebUI.maximizeWindow()
 
-WebUI.navigateToUrl('k')
+WebUI.navigateToUrl('lll')
 
 WebUI.acceptAlert()
 
